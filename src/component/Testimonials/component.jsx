06f26f19264f4d16bbc731c93f683b21e065243c
Mirazamn <mirsaidovmirazam@gmail.com>
2025-06-11 09:11:44 +0500

    Testimonals lang corrected

--- src/component/Testimonials/component.jsx
+++ src/component/Testimonials/component.jsx
@@ -2,13 +2,16 @@ import './style.css'
 
 import { FaStar } from "react-icons/fa";
 import TestimonialsSvg from '../../assets/Testimonials.svg'
+import { useTranslation } from 'react-i18next';
 
 function Testimonials() {
+  const {t} = useTranslation()
+
   return (
     <section className="testimonials">
         <div className="container flex">
             <div className="content">
-                <h2>Testimonials</h2>
+                <h2>{t("testimonials.title")}</h2>
                 <div className="raiting flex">
                     <FaStar className='icon'/>
                     <FaStar className='icon'/>
@@ -16,9 +19,9 @@ function Testimonials() {
                     <FaStar className='icon'/>
                     <FaStar className='icon'/>
                 </div>
-                <p>“Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”</p>
-                <h4>Edward Newgate</h4>
-                <h6>Founder Circle</h6>
+                <p>{t("testimonials.quote")}</p>
+                <h4>{t("testimonials.name")}</h4>
+                <h6>{t("testimonials.position")}</h6>
             </div>
 
             <img src={TestimonialsSvg} alt="" />
